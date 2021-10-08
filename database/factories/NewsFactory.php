7e910d478f32e9a9de8182116a72b51c9aaed071
Chelove4ik <32807164+Chelove4ik@users.noreply.github.com>
2021-10-08 22:21:16 +1000

    Lesson 3: Add news model, migration, factory and seeder (#2)
    * Lesson 3: Установлен cviebrock/eloquent-sluggable
    
    * Lesson 3: Add news model, migration, factory and seeder
    
    * Lesson 3: fix. Удалено ненужное заполение `created_at` и `updated_at`

---
+++ database/factories/NewsFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\News;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class NewsFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = News::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->unique->text,
+            'text' => $this->faker->text,
+            'is_published' => $this->faker->boolean(70),
+            'published_at' => $this->faker->dateTimeInInterval('-2 months', 'now'),
+        ];
+    }
+}
